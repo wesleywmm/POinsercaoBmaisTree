421f6f951a6c344fe48580d4ebd90bdd81071ad8
WesleyMiranda <wesleywmm@hotmail.com>
2023-06-12 22:06:41 -0300

    Criacao Metodos De Busca

--- BMaisTree.java
+++ BMaisTree.java
@@ -7,6 +7,44 @@ public class BMaisTree implements Constantes {
         raiz = null;
     }
 
-    
+    private No navegarAteFolha(int info)
+    {
+        int pos;
+        No no=raiz;
+        while(no.getvLig(0) != null)
+        {
+            pos = no.procurarPosicao(info);
+            no = no.getvLig(pos);
+        }
+        return no;
+    }
+
+    private No localizarPai(No folha, int info)
+    {
+        int pos;
+        No no=raiz, pai=raiz;
+        while(no!=folha)
+        {
+            pai=no;
+            pos = no.procurarPosicao(info);
+            no = no.getvLig(pos);
+        }
+        return pai;
+    }
+
+    private No localizarNo(int info)
+    {
+        No no=raiz;
+        boolean achou=false;
+        while(no!=null && !achou)
+        {
+            int pos=no.procurarPosicao(info);
+            if(pos<no.getTl() && no.getvInfo(pos)==info)
+                achou=true;
+            else
+                no = no.getvLig(pos);
+        }
+        return no;
+    }
 
 }
